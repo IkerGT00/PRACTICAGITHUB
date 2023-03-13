67264eec65246ad8ee9784675567c1b76dd698b8
Iker <Ikergt@outlook.es>
2023-03-13 13:35:28 +0100

    Segundo commit

--- EjemploGit/src/App.java
+++ EjemploGit/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("ESTA ES UNA PRÁCTICA GIT.");
+        System.err.println("AHORA HE DE ACTUALIZAR.");
     }
 }
