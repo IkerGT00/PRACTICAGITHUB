01cd0f401ac66862d6659752be460e0598100fe6
IkerGT00 <113137205+IkerGT00@users.noreply.github.com>
2023-03-23 12:35:08 +0100

    Update App.java

--- EjemploGit/src/App.java
+++ EjemploGit/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("ESTA ES UNA PRÁCTICA GIT.");
-        System.err.println("AHORA HE DE ACTUALIZAR.");
+        System.out.println("AHORA HE DE ACTUALIZAR.");
+        System.out.println("ACTUALIZADO EN EL REMOTO.");
     }
 }
